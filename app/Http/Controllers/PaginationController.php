b0db50e87c5a606322dd5081c0204cd01ee9c8de
Marino Giudice <56487981+marinogiudice@users.noreply.github.com>
2023-05-05 11:44:53 +0100

    Update PaginationController.php

--- app/Http/Controllers/PaginationController.php
+++ app/Http/Controllers/PaginationController.php
@@ -7,7 +7,9 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 /**
  * The class performs pagination operations on arrays or collections .
- * It's used to paginated the results from the categories database table
+ * It's used to paginated the results from the categories or products database table
+ * 
+ * @author Marino Giudice
  * 
  */
 
@@ -21,7 +23,7 @@ class PaginationController extends Controller
 
     
     // Create a new  collection from data
-    $productCollection = collect($data);
+    $itemsCollection = collect($data);
 
     // Defines the page length
     $perPage = 10;
@@ -30,18 +32,15 @@ class PaginationController extends Controller
     $currentPage = LengthAwarePaginator::resolveCurrentPage();
 
     // Chuncks the collection and injects it in the current page
-    $currentCategoryPage = $productCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
+    $currentItemsPage = $itemsCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
 
     // Creates a paginator and pass it to the view with the current chunck for the current page
-    $paginatedCategories= new LengthAwarePaginator($currentCategoryPage , count($productCollection), $perPage);
+    $paginatedItems= new LengthAwarePaginator($currentItemsPage , count($itemsCollection), $perPage);
 
     // generates the results pages urls
-    $paginatedCategories->setPath($request->url());
+    $paginatedItems->setPath($request->url());
     
 
-    return $paginatedCategories;
-    //return view('categories', ['data' => $paginatedCategories]);
-
-
+    return $paginatedItems;
     }
 }
